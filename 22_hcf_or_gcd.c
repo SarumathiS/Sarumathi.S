1bbf9603baba93173937eb8279ee5b19911c82a5
SarumathiS <sarusm3796@gmail.com>
2016-06-06 16:10:00 +0530

    Create 22_hcf_or_gcd.c

---
+++ 22_hcf_or_gcd.c
@@ -0,0 +1,16 @@
+ #include<stdio.h>
+ int gcd(int a, int b)
+{
+    if (a == b)
+        return a;
+    if (a > b)
+        return gcd(a-b, b);
+    return gcd(a, b-a);
+}
+ main()
+ {
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("GCD of %d and %d is %d ",a, b, gcd(a, b));
+    return 0;
+    }
