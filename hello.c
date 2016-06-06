92fb54a65bfb90619395c38e90483e9cafa92ecb
SarumathiS <sarusm3796@gmail.com>
2016-06-06 14:14:04 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+     
+      int n,ln,i;
+      scanf("%d",&n);
+       char str[n];
+      gets(str);
+      ln=strlen(str)-1;
+      for(i=0;i<ln;i++)
+      {
+          printf("\nHello");
+      }
+
+}
