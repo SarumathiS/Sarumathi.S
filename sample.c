89534aaeab52a1d3570b8d1d2fd646a556bb82fb
SarumathiS <sarusm3796@gmail.com>
2016-06-06 12:08:47 +0530

    Update sample.c

--- sample.c
+++ sample.c
@@ -2,6 +2,15 @@
 
 int main()
 {
-    printf("Hello World!\n");
+    int n;
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("The number is positive");
+    }
+    else
+    {
+        printf("The number is negative");
+    }
     return 0;
 }
