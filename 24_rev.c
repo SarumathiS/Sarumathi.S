f1ab8bdbf105c913ad2d69e86b7967b45ee787b8
SarumathiS <sarusm3796@gmail.com>
2016-06-06 20:15:33 +0530

    Create 24_rev.c

---
+++ 24_rev.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, rev_num=0, remainder;
+    scanf("%d", &n);
+
+    while(n!= 0)
+    {
+        remainder = n%10;
+        rev_num=rev_num*10+remainder;
+        n/=10;
+    }
+
+    printf("Reversed Number=%d",rev_num);
+    return 0;
+}
