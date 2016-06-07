beadbcebfed8142183dd5aa74a8a614343979e82
SarumathiS <sarusm3796@gmail.com>
2016-06-07 14:05:14 +0530

    Create 45_successentry.c

---
+++ 45_successentry.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+   int n,range,i;
+   scanf("%d %d",&n,&range);
+       if(n>range)
+       {
+           printf("Error enter an valid input");
+       }
+       else
+       {
+   printf("Successfull entry of a number %d",n);
+       }
+        return 0;
+}
