9791343cc4b356a740d20988ade457894d362855
SarumathiS <sarusm3796@gmail.com>
2016-06-07 10:15:58 +0530

    Update 60_max_and_min_sum.c

--- 60_max_and_min_sum.c
+++ 60_max_and_min_sum.c
@@ -2,7 +2,7 @@
 main()
 {
     int arr[100];
-    int i, max, min, size;
+    int i, max, min, size,res;
     scanf("%d", &size);
     for(i=0; i<size; i++)
     {
@@ -24,6 +24,9 @@ main()
      
     printf("Maximum element = %d\n", max);
     printf("Minimum element = %d", min);
+    res=max+min;
+    printf("The sum of max and min is:");
+    printf("%d",res);
     return 0;
     
 }
